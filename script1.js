a9962a7672934ae5ce9fa4f09d2385772fa3ce7f
Jair <jchepetla@gmail.com>
2015-10-01 17:00:49 -0700

    first commit

---
+++ script1.js
@@ -0,0 +1,28 @@
+//Assignment 3, Part 1
+//STEP 1
+/*
+var favMovies = new Array ("Warrior", "Terminator", "Dumb and Dumber", "Lord Of The Rings", "Star Wars");
+console.log(favMovies[1]);
+*/
+
+//STEP 2
+/*
+var movies = [];
+movies [0] = "Warrior";
+movies [1] = "Terminator";
+movies [2] = "Dumb and Dumber";
+movies [3] = "Lord Of The Rings";
+movies [4] = "Star Wars";
+console.log(movies[0]);
+*/
+
+//STEP 3
+var movies = [];
+movies [0] = "Warrior";
+movies [1] = "Terminator";
+movies [2] = "Dumb and Dumber";
+movies [3] = "Lord Of The Rings";
+movies [4] = "Star Wars";
+
+movies [2] = "Anchorman"; 
+console.log(movies.length);
